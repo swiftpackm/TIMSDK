a8b9105f0cdd9eaa106fa975fb368428951788f5
xiusl <xiushilin@hotmail.com>
2025-01-17 17:05:42 +0800

    add TIMPush

--- Package.swift
+++ Package.swift
@@ -9,14 +9,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TIMSDK",
-            targets: ["TIMSDK", "ImSDK_Plus_Swift"]),
+            targets: ["TIMSDK", "ImSDK_Plus_Swift", "TIMPush"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "TIMSDK"),
-        .binaryTarget(name: "ImSDK_Plus_Swift", path: "./Frameworks/ImSDK_Plus_Swift.xcframework")
+        .binaryTarget(name: "ImSDK_Plus_Swift", path: "./Frameworks/ImSDK_Plus_Swift.xcframework"),
+        .binaryTarget(name: "TIMPush",
+                      url: "https://im.sdk.qcloud.com/download/tuiplugin/8.3.6498/ios/TPush_8.3.6498.xcframework.zip",
+                      checksum: "f7bb45113f605f7711fa42affd7e33d6")
 
     ]
 )
