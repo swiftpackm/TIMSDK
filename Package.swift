bfcc4f6599885a3174339c5e92568c98bc1c9a70
士林 <xiushilin@hotmail.com>
2025-01-17 17:58:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TIMSDK",
-            targets: ["TIMSDK", "ImSDK_Plus_Swift", "TIMPush"]),
+            targets: ["TIMSDK", "ImSDK_Plus_Swift", "TPush"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -17,7 +17,7 @@ let package = Package(
         .target(
             name: "TIMSDK"),
         .binaryTarget(name: "ImSDK_Plus_Swift", path: "./Frameworks/ImSDK_Plus_Swift.xcframework"),
-        .binaryTarget(name: "TIMPush",
+        .binaryTarget(name: "TPush",
                       url: "https://im.sdk.qcloud.com/download/tuiplugin/8.3.6498/ios/TPush_8.3.6498.xcframework.zip",
                       checksum: "ea1324b532e7af264f5bc716b9b68276afde9c77d0d246409057be2715d6f932")
 
